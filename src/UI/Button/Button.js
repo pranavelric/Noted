85def2b497fc7150d805b8dc1112b0811db58e14
Pranav Choudhary <2018184@iiitdmj.ac.in>
2022-06-17 23:19:47 +0530

    Button component added

---
+++ src/UI/Button/Button.js
@@ -0,0 +1,52 @@
+import React, {useState} from 'react'
+import './button.scss';
+export default function Button({title,altText,children,classes,clicked,texted,small}) {
+    const [hovered,setHovered] = useState(false);
+    const handleHover=()=>{
+        setHovered(!hovered);
+    };
+    if(texted){
+        return (
+            <button
+            className={`texted-button ${classes}`}
+            onClick={clicked}
+          >
+            {title}
+          </button>
+        );
+    }
+    else
+    {
+
+
+        return (
+            <button
+              className={`button ${
+                small ? 'h-8 w-8' : 'h-10 w-10'
+              }  ${classes}`}
+              onClick={clicked}
+              onMouseEnter={handleHover}
+              onMouseLeave={handleHover}
+            >
+              {children}
+              {altText && hovered && (
+                <div
+                  className={`${
+                    small ? 'mt-8' : 'mt-10'
+                  } btn-child`}
+                >
+                  {altText}
+                </div>
+              )}
+            </button>
+          );
+
+
+
+
+
+
+    }
+   
+};
+
