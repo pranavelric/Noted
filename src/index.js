350bf1248d1949477dff23168074d03b160eabf1
Pranav Choudhary <2018184@iiitdmj.ac.in>
2022-06-20 21:46:02 +0530

    Changes to make gh-pages work

--- src/index.js
+++ src/index.js
@@ -11,10 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HashRouter  >
-      <Routes>
-
-            <Route exact path = '/' element={<App />} />
-      </Routes>
+        <App />
     </HashRouter>
   </React.StrictMode>
 );
